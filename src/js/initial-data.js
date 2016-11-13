6645f63188d095b0b2d77a2210f533481764b0f3
Bryce Watson <brywason@ebay.com>
2016-11-13 13:27:24 -0800

    Style improvements, adding comments, updating README

--- src/js/initial-data.js
+++ src/js/initial-data.js
@@ -1,38 +1,38 @@
 module.exports = {
-    getData: function() {
-        let data = {
-            playerOne: {
-                playerNumber: 1,
-                isHuman: true,
-                wins: 0
-            },
-            playerTwo: {
-                playerNumber: 2,
-                isHuman: false,
-                wins: 0
-            },
-            scoreboard: {
-                totalGames: 5,
-                bestOf: 3,
-                currentGame: 0,
-                games: []
-            },
-            choices: {
-                rock: {
-                    id: 'rock',
-                    beats: ['scissors']
-                },
-                paper: {
-                    id: 'paper',
-                    beats: ['rock']
-                },
-                scissors: {
-                    id: 'scissors',
-                    beats: ['paper']
-                }
-            }
-        };
-        data.playerOne.choices = data.choices;
-        return data;
-    }
+	getData: function() {
+		let data = {
+			playerOne: {
+				playerNumber: 1,
+				isHuman: true,
+				wins: 0
+			},
+			playerTwo: {
+				playerNumber: 2,
+				isHuman: false,
+				wins: 0
+			},
+			scoreboard: {
+				totalGames: 5,
+				bestOf: 3,
+				currentGame: 0,
+				games: []
+			},
+			choices: {
+				rock: {
+					id: 'rock',
+					beats: ['scissors']
+				},
+				paper: {
+					id: 'paper',
+					beats: ['rock']
+				},
+				scissors: {
+					id: 'scissors',
+					beats: ['paper']
+				}
+			}
+		};
+		data.playerOne.choices = data.choices;
+		return data;
+	}
 };
--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,12 @@ import React from 'react';
 import {render} from 'react-dom';
 import ROCK_PAPER_SCISSORS from '../modules/rock-paper-scissors';
 
+/**
+ * Loads the initial state data for the app.
+ * @return {Object} Initial state object
+ */
 function getInitialState() {
-    return require('./initial-data.js').getData();
+	return require('./initial-data.js').getData();
 }
+
 render(React.createElement(ROCK_PAPER_SCISSORS, getInitialState()), document.getElementById('content'));
